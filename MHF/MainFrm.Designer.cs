33df62c545f0305c9999de01630ff73b3feccecd
Andrew Cavallo <65136929+Awire9966@users.noreply.github.com>
2024-10-05 20:43:28 -0400

    MHF V1.0

---
+++ MHF/MainFrm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace MHF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rb = new System.Windows.Forms.Button();
+            this.fu = new System.Windows.Forms.Label();
+            this.thewordstatus_duh = new System.Windows.Forms.Label();
+            this.status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rb
+            // 
+            this.rb.Location = new System.Drawing.Point(12, 75);
+            this.rb.Name = "rb";
+            this.rb.Size = new System.Drawing.Size(286, 23);
+            this.rb.TabIndex = 0;
+            this.rb.Text = "RESET!";
+            this.rb.UseVisualStyleBackColor = true;
+            this.rb.Click += new System.EventHandler(this.rb_Click);
+            // 
+            // fu
+            // 
+            this.fu.AutoSize = true;
+            this.fu.Location = new System.Drawing.Point(12, 22);
+            this.fu.Name = "fu";
+            this.fu.Size = new System.Drawing.Size(76, 13);
+            this.fu.TabIndex = 1;
+            this.fu.Text = "x/20 files used";
+            // 
+            // thewordstatus_duh
+            // 
+            this.thewordstatus_duh.AutoSize = true;
+            this.thewordstatus_duh.Location = new System.Drawing.Point(12, 35);
+            this.thewordstatus_duh.Name = "thewordstatus_duh";
+            this.thewordstatus_duh.Size = new System.Drawing.Size(40, 13);
+            this.thewordstatus_duh.TabIndex = 2;
+            this.thewordstatus_duh.Text = "Status:";
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.ForeColor = System.Drawing.Color.Green;
+            this.status.Location = new System.Drawing.Point(53, 35);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(41, 13);
+            this.status.TabIndex = 3;
+            this.status.Text = "Ready!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 110);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.thewordstatus_duh);
+            this.Controls.Add(this.fu);
+            this.Controls.Add(this.rb);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MediaHuman Fucker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button rb;
+        private System.Windows.Forms.Label fu;
+        private System.Windows.Forms.Label thewordstatus_duh;
+        private System.Windows.Forms.Label status;
+    }
+}
+
---
+++ MHF/MainFrm.cs
@@ -0,0 +1,50 @@
+﻿using MHF.Properties;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MHF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int used;
+        RegistryKey ck = Registry.CurrentUser.OpenSubKey("Software\\MediaHuman\\Video Converter", true);
+        private void loadvalues()
+        {
+            used = (int)ck.GetValue("FilesDone");
+            fu.Text = fu.Text.Replace("x", used.ToString());
+            if (used == 0)
+            {
+                status.Text = "Already at lowest possible value!";
+                status.ForeColor = Color.Red;
+                rb.Enabled = false;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show(Resources.msgtouser, "Hi!");
+            loadvalues();   
+        
+        }
+
+        private void rb_Click(object sender, EventArgs e)
+        {
+            Process.Start("taskkill", "/f /IM MHVideoConverter.exe");
+            ck.SetValue("FilesDone", 0);
+            used = 0;
+            loadvalues();
+        }
+    }
+}
